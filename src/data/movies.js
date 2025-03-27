9af89e2da0a6160e648213b2e91b7e4deda5fb7a
NavodContrath <navod.contrath@gmail.com>
2025-03-27 20:50:05 +0100

    add images to movie entries in the movies data

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,9 +1,9 @@
 const movies = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
+    { title: 'Inception', genre: 'Fantascienza', img: '/img/inception.jpg' },
+    { title: 'Il Padrino', genre: 'Thriller', img: '/img/padrino.jpg' },
+    { title: 'Titanic', genre: 'Romantico', img: '/img/titanic.jpg' },
+    { title: 'Batman', genre: 'Azione', img: '/img/batman.jpg' },
+    { title: 'Interstellar', genre: 'Fantascienza', img: '/img/interstellar.jpg' },
+    { title: 'Pulp Fiction', genre: 'Thriller', img: '/img/pulp-fiction.jpg' },
 ]
 export default movies

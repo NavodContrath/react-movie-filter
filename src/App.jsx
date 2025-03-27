87778c6dc6fc98073f483b4b441c367de801cb55
NavodContrath <navod.contrath@gmail.com>
2025-03-27 15:59:09 +0100

    add movie list display and genre selection form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 import { useState } from 'react'
+import movies from './data/movies'
 
 
 function App() {
+  const [movieList, setMovieList] = useState(movies)
+
 
 
   return (
-    <>
+    <div className='container'>
+      {
+        movieList.map((movie, i) => {
+          return (
+            <div key={`movie-${i}`}>
+              <div>
+                {movie.title}
+              </div>
+              <div>
+                {movie.genre}
+              </div>
+            </div>
 
-    </>
+          )
+        })
+      }
+      <form>
+        <label htmlFor="genres">chose a genre</label>
+        <select name='movies-genre' id="movies-genre">
+          {
+            movieList.map((movie, i) => {
+              return (
+                <option key={i} value={movie.genre}>{movie.genre}</option>
+              )
+            })
+
+          }
+        </select>
+        <input type="submit" value="Submit" />
+
+
+      </form>
+
+    </div>
   )
 }
 
